9aca8073d96c6c67e994701eb3e0230b00601141
Skyfighter64 <skyfighter64@protonmail.com>
2025-10-14 23:29:10 +0200

    add video to lightshow converter

---
+++ video_to_lightshow.py
@@ -0,0 +1,16 @@
+# generate a lightshow json based a video file
+
+
+def main():
+    pass
+    # 1. read in video frames
+    # 2. specify the devices and number of leds per device 
+    # 3. specify from where on the image each device should sample the pixel colors
+    # 4. go through video file and sample pixel colors into json
+
+
+
+
+
+if __name__ == "__main__":
+    main()
