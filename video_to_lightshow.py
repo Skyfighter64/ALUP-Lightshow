9c79dd10f2f458f3ee1a04c2f9869426569f21c9
Skyfighter64 <skyfighter64@protonmail.com>
2025-10-20 18:49:49 +0200

    Parse LED arrangement from bitmaps

--- video_to_lightshow.py
+++ video_to_lightshow.py
@@ -19,6 +19,9 @@ Simple script turning a mp4 video file into a lightshow JSON which can be used w
 Scales the given video down to one horizontal line of NUM_LED pixels. Only works for a single device.
 
 """
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.info)
 
 parser = argparse.ArgumentParser(prog="Video To Lightshow", description="Convert video files to ALUP light shows which can be played with the light show player")
 # setup arg parser
@@ -26,7 +29,9 @@ parser.add_argument('video_file', help="Specify a video file to create a lightsh
 parser.add_argument('-n', '--num_leds', default=100, type=int, help="The number of LEDs of the LED strip. Will define the number of colors inside each frame.")      # option that takes a value
 parser.add_argument('-o', '--output', default='output.json', help="The output json file to which the light show will be written. Default: 'output.json'")      # option that takes a value
 parser.add_argument('-v', '--verbose', action='store_true', help="Enable verbose logging")  # on/off flag
-parser.add_argument('--supress_live_view', action='store_true', help="Disable the live viewing window")
+parser.add_argument('--suppress_live_view', action='store_true', help="Disable the live viewing window")
+
+
 def main():
     # handle cmdline args
     args = parser.parse_args()
@@ -34,7 +39,14 @@ def main():
     if(args.verbose):
         logging.basicConfig()
         logging.getLogger().setLevel(logging.DEBUG)
+        logger.setLevel(logging.DEBUG)
 
+
+    # read in LED arrangement
+    arrangement, arrangement_shape = ArrangementFromBitmap("arrangements/zigzag.bmp")
+    mask = MaskFromArrangement(arrangement, arrangement_shape)
+
+    print(mask)  
     cap = cv2.VideoCapture(args.video_file)
     show = Lightshow()
     show.frames = [[]] # initialize frames for one device
@@ -51,14 +63,24 @@ def main():
             print("Video end reached.")
             break
 
-        # rescale frame to n x 1 resolution
-        resized_frame = cv2.resize(frame, (args.num_leds,1))
-        AddFrameToLightshow(show, list(resized_frame)[0], timestamp)
+        # rescale frame to the same resolution as the arrangement
+        resized_frame = cv2.resize(frame, arrangement_shape)
+        resized_frame = cv2.bitwise_and(resized_frame, mask)
+
+        # sample from the frame based on the LED positions defined in the arrangement
+        colors = SampleFromFrame(resized_frame, arrangement)
+
+        # add the colors of this frame to the light show
+        AddFrameToLightshow(show, colors, timestamp)
 
         # provide a live view of what's currently processed
-        if not args.supress_live_view:
-            reresized_frame = cv2.resize(resized_frame, None, fx=25, fy = 25)
+        if not args.suppress_live_view:
+            reresized_frame = cv2.resize(resized_frame, None, fx=25, fy = 25, interpolation = cv2.INTER_NEAREST)
             cv2.imshow('frame', reresized_frame)
+
+            # show the raw color output for debug purposes
+            if (logger.level <= logging.DEBUG):
+                cv2.imshow("colors", cv2.resize(np.array([colors]), None, fx=25, fy = 25, interpolation = cv2.INTER_NEAREST))
 
         if cv2.waitKey(1) == ord('q'):
             break
@@ -66,10 +88,71 @@ def main():
     # close all cv2 related stuff
     cap.release()
     cv2.destroyAllWindows()
-    print("Converting to JSON")
+    logger.info("Converting to JSON")
     # export the lightshow as json
     show.toJson(args.output)
-    print("Done")
+    logger.info("Done")
+
+
+# create a filter mask for 
+def MaskFromArrangement(arrangement, shape):
+    mask = np.zeros((shape[1], shape[0], 3), dtype=np.uint8)
+    for led in arrangement:
+        x = led[1]
+        y = led[2]
+        mask[y][x] = [255, 255, 255]
+
+    return mask
+
+# sample from a frame using the given arrangement tuples (index, x, y)
+def SampleFromFrame(frame, arrangement):
+    colors = [None for _ in range(len(arrangement))]
+    for led in arrangement:
+        index = led[0]
+        x = led[1]
+        y = led[2]
+        colors[index] = frame[y][x]
+
+    return colors
+
+
+
+# get the LED arrangement from a bitmap where the integer color value
+# represents the index of the LED at a 2d position
+# eg: the position at the pixel with color 0x000001 will be the position at which the color of the LED with index 1 is sampled.
+# returns list((index, x, y)), (width, height)
+def ArrangementFromBitmap( file):
+    image = cv2.imread(file)
+
+    if image is None:
+        logger.error("Could not read Arrangement from file " + str(file))
+        return [], (0,0)
+
+    # convert the shape to (width, height)
+    shape = (image.shape[1], image.shape[0])
+
+    # read in all pixels and add coordinates to LED mask
+    arrangement = []
+
+    # extract the coordinates for each led from the image
+    for y in range(shape[1]):
+        for x in range(shape[0]):
+            # convert the color of the pixel to an RGB index
+            index = RGBToInt(image[y][x])
+
+            # ignore white pixels
+            if (index == 0xffffff):
+                continue
+
+            arrangement.append((index, x, y))
+    return arrangement, shape
+
+def RGBToInt(rgb):
+    color = 0
+    for c in rgb[::-1]:
+        color = (color<<8) + c
+    return color
+
 
 
 
@@ -84,6 +167,8 @@ def AddFrameToLightshow(lightshow, colors, timestamp):
     frame.timestamp = timestamp
     # NOTE: currently, this will only work for one single device
     lightshow.frames[0].append(frame)
+
+
 
 
 
